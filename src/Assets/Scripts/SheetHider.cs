01d2fafc07dc0d38849b90ed68d796b680e18075
Felix Stumvoll <felix.stumvoll@gmail.com>
2020-12-19 15:34:20 +0100

    adds remove behaviour to sheet

--- src/Assets/Scripts/SheetHider.cs
+++ src/Assets/Scripts/SheetHider.cs
@@ -7,12 +7,11 @@ public class SheetHider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<Renderer>().enabled = false;
     }
 }
