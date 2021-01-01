8f5a5b4837ab99c0773095a83aafb7815ef13966
Felix Stumvoll <felix.stumvoll@gmail.com>
2021-01-01 22:32:20 +0100

    give me some nameSPACE

--- src/Assets/Scripts/Behaviour/InfoBarTextBehaviour.cs
+++ src/Assets/Scripts/Behaviour/InfoBarTextBehaviour.cs
@@ -8,121 +8,124 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InfoBarTextBehaviour : MonoBehaviour
+namespace Assets.Scripts.Behaviour
 {
-    public Font infoBarFont;
-
-    private const string MistakesInfoTextTemplate = "Mistakes: {0} (minor) / {1} (fatal)";
-    private const string CWBInfoTextTemplate = "Bleeped Curse Words: {0}";
-
-    private (GameObject GO, Text Text)? mistakesInfoText;
-    private (GameObject GO, Text Text)? cwbInfoText;
-
-    public void Start()
+    public class InfoBarTextBehaviour : MonoBehaviour
     {
-        var settingsComponent = ComponentConfig.Instance.GetService<ISettingsComponent>();
-        var spellChecker = ComponentConfig.Instance.GetService<ISpellChecker>();
-        var curseWordChecker = ComponentConfig.Instance.GetService<ICurseWordChecker>();
+        public Font infoBarFont;
 
-        spellChecker.OnMistakesFound +=
-            mistakes =>
-            {
-                this.UpdateSpellingMistakeInfoText(mistakes);
-                this.RealignInfoTexts();
-            };
+        private const string MistakesInfoTextTemplate = "Mistakes: {0} (minor) / {1} (fatal)";
+        private const string CWBInfoTextTemplate = "Bleeped Curse Words: {0}";
 
-        curseWordChecker.OnCurseWordsExtracted +=
-            curseWords =>
-            {
-                this.UpdateCurseWordInfoText(curseWords);
-                this.RealignInfoTexts();
-            };
+        private (GameObject GO, Text Text)? mistakesInfoText;
+        private (GameObject GO, Text Text)? cwbInfoText;
 
-        settingsComponent.OnIsCWBEnabledChanged +=
-            isCwbEnabled =>
-            {
-                if (this.cwbInfoText != null)
+        public void Start()
+        {
+            var settingsComponent = ComponentConfig.Instance.GetService<ISettingsComponent>();
+            var spellChecker = ComponentConfig.Instance.GetService<ISpellChecker>();
+            var curseWordChecker = ComponentConfig.Instance.GetService<ICurseWordChecker>();
+
+            spellChecker.OnMistakesFound +=
+                mistakes =>
                 {
-                    this.cwbInfoText.Value.GO.SetActive(isCwbEnabled);
-                }
+                    this.UpdateSpellingMistakeInfoText(mistakes);
+                    this.RealignInfoTexts();
+                };
 
-                this.RealignInfoTexts();
-            };
-    }
+            curseWordChecker.OnCurseWordsExtracted +=
+                curseWords =>
+                {
+                    this.UpdateCurseWordInfoText(curseWords);
+                    this.RealignInfoTexts();
+                };
 
-    private void UpdateSpellingMistakeInfoText(IEnumerable<SpellingMistake> mistakes)
-    {
-        if (this.mistakesInfoText == null)
-        {
-            var gameObject = new GameObject("Mistakes InfoText");
-            gameObject.transform.SetParent(this.transform);
-            gameObject.transform.position = this.transform.position;
-            gameObject.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
-            gameObject.transform.localScale = new Vector3(1f, 0.02f, 1f);
+            settingsComponent.OnIsCWBEnabledChanged +=
+                isCwbEnabled =>
+                {
+                    if (this.cwbInfoText != null)
+                    {
+                        this.cwbInfoText.Value.GO.SetActive(isCwbEnabled);
+                    }
 
-            var text = gameObject.AddComponent<Text>();
-            text.font = this.infoBarFont;
-            text.fontSize = 85;
-            text.verticalOverflow = VerticalWrapMode.Overflow;
-            text.horizontalOverflow = HorizontalWrapMode.Overflow;
-            text.alignment = TextAnchor.MiddleCenter;
-
-            this.mistakesInfoText = (gameObject, text);
+                    this.RealignInfoTexts();
+                };
         }
 
-        this.mistakesInfoText.Value.Text.text = string.Format(
-            MistakesInfoTextTemplate,
-            mistakes.Count(m => m.Severity == SpellingSeverity.Minor),
-            mistakes.Count(m => m.Severity == SpellingSeverity.Fatal));
-    }
-
-    private void UpdateCurseWordInfoText(IEnumerable<CurseWord> curseWords)
-    {
-        if (this.cwbInfoText == null)
+        private void UpdateSpellingMistakeInfoText(IEnumerable<SpellingMistake> mistakes)
         {
-            var gameObject = new GameObject("CWB InfoText");
-            gameObject.AddComponent<Renderer>();
-
-            gameObject.transform.SetParent(this.transform);
-            gameObject.transform.position = this.transform.position;
-            gameObject.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
-            gameObject.transform.localScale = new Vector3(1f, 0.02f, 1f);
-
-            var text = gameObject.AddComponent<Text>();
-            text.font = this.infoBarFont;
-            text.fontSize = 85;
-            text.verticalOverflow = VerticalWrapMode.Overflow;
-            text.horizontalOverflow = HorizontalWrapMode.Overflow;
-            text.alignment = TextAnchor.MiddleCenter;
-
-            this.cwbInfoText = (gameObject, text);
-        }
-
-        this.cwbInfoText.Value.Text.text = string.Format(
-            CWBInfoTextTemplate,
-            curseWords.Count());
-    }
-
-    private void RealignInfoTexts()
-    {
-        if (this.cwbInfoText.HasValue
-            && this.cwbInfoText.Value.GO.activeSelf)
-        {
-            if (this.mistakesInfoText.HasValue)
+            if (this.mistakesInfoText == null)
             {
-                this.mistakesInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 1.25f);
-                this.cwbInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, -1.25f);
+                var gameObject = new GameObject("Mistakes InfoText");
+                gameObject.transform.SetParent(this.transform);
+                gameObject.transform.position = this.transform.position;
+                gameObject.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+                gameObject.transform.localScale = new Vector3(1f, 0.02f, 1f);
+
+                var text = gameObject.AddComponent<Text>();
+                text.font = this.infoBarFont;
+                text.fontSize = 85;
+                text.verticalOverflow = VerticalWrapMode.Overflow;
+                text.horizontalOverflow = HorizontalWrapMode.Overflow;
+                text.alignment = TextAnchor.MiddleCenter;
+
+                this.mistakesInfoText = (gameObject, text);
+            }
+
+            this.mistakesInfoText.Value.Text.text = string.Format(
+                MistakesInfoTextTemplate,
+                mistakes.Count(m => m.Severity == SpellingSeverity.Minor),
+                mistakes.Count(m => m.Severity == SpellingSeverity.Fatal));
+        }
+
+        private void UpdateCurseWordInfoText(IEnumerable<CurseWord> curseWords)
+        {
+            if (this.cwbInfoText == null)
+            {
+                var gameObject = new GameObject("CWB InfoText");
+                gameObject.AddComponent<Renderer>();
+
+                gameObject.transform.SetParent(this.transform);
+                gameObject.transform.position = this.transform.position;
+                gameObject.transform.localEulerAngles = new Vector3(90f, 0f, 0f);
+                gameObject.transform.localScale = new Vector3(1f, 0.02f, 1f);
+
+                var text = gameObject.AddComponent<Text>();
+                text.font = this.infoBarFont;
+                text.fontSize = 85;
+                text.verticalOverflow = VerticalWrapMode.Overflow;
+                text.horizontalOverflow = HorizontalWrapMode.Overflow;
+                text.alignment = TextAnchor.MiddleCenter;
+
+                this.cwbInfoText = (gameObject, text);
+            }
+
+            this.cwbInfoText.Value.Text.text = string.Format(
+                CWBInfoTextTemplate,
+                curseWords.Count());
+        }
+
+        private void RealignInfoTexts()
+        {
+            if (this.cwbInfoText.HasValue
+                && this.cwbInfoText.Value.GO.activeSelf)
+            {
+                if (this.mistakesInfoText.HasValue)
+                {
+                    this.mistakesInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 1.25f);
+                    this.cwbInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, -1.25f);
+                }
+                else
+                {
+                    this.cwbInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 0f);
+                }
             }
             else
             {
-                this.cwbInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 0f);
-            }
-        }
-        else
-        {
-            if (this.mistakesInfoText.HasValue)
-            {
-                this.mistakesInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 0f);
+                if (this.mistakesInfoText.HasValue)
+                {
+                    this.mistakesInfoText.Value.GO.transform.localPosition = new Vector3(0f, 0f, 0f);
+                }
             }
         }
     }
