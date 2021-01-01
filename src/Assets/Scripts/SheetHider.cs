5b77cdcf2a49b7bd49475eea3d2efdf3f487aa1a
Manuel Fuchs <manuel.fuchs@softaware.at>
2021-01-01 17:57:34 +0100

    implement InfoBar behaviour script

--- src/Assets/Scripts/SheetHider.cs
+++ src/Assets/Scripts/SheetHider.cs
@@ -5,13 +5,6 @@ namespace Assets.Scripts
 {
     public class SheetHider : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            var spellChecker = ComponentConfig.Instance.GetService<ISpellChecker>();
-            Debug.LogWarning($"Loaded {spellChecker.GetType().FullName}");
-        }
-
         // Update is called once per frame
         void Update()
         {
